0e7dd1f08ebb1e60e9efa61dfaf570f6c0de2bb2
Ahlou1713 <98119853+Ahlou1713@users.noreply.github.com>
2024-02-03 18:19:55 +0100

    Time: 37 ms (52.91%) | Memory: 16.5 MB (71.80%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.py
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.py
@@ -1,3 +1,4 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
+        #The function 'replace' replaces the first given string by the second given string
         return address.replace('.','[.]')
