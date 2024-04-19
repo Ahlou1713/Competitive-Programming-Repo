9d3c7c2e4537ca40d4d7a6a94037ddfc1c41f1ca
Ahlou1713 <98119853+Ahlou1713@users.noreply.github.com>
2024-04-19 19:30:18 +0100

    Time: 479 ms (20.78%) | Memory: 18.5 MB (47.92%) - LeetSync

--- 147-insertion-sort-list/insertion-sort-list.py
+++ 147-insertion-sort-list/insertion-sort-list.py
@@ -3,50 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# class Solution:
-#     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-#         dummy = ListNode()
-
-#         dummy2 = ListNode()
-#         dummy2.next = head
-#         curr = dummy2
-#         while curr.next:
-#             prev = dummy
-#             while prev.next and curr.next.val > prev.next.val:
-#                 prev = prev.next
-            
-#             # Placing the element
-#             next_node = prev.next
-#             prev.next = curr.next
-#             another_next_node = curr.next.next
-#             curr.next.next = next_node
-#             curr.next = another_next_node
-
-#         return dummy.next
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
-        back = head 
-        front = head.next 
-
-        stop = 0
-
-        while front:
-            if back.val > front.val:
-                back.val, front.val = front.val, back.val 
-            back = back.next 
-            front = front.next 
-            stop += 1
         
-        for _ in range(stop): 
-            back = head 
-            front = head.next 
+        dummy = ListNode()
 
-            while front:
-                if back.val > front.val:
-                    back.val, front.val = front.val, back.val 
-                back = back.next 
-                front = front.next
+        dummy2 = ListNode()
+        dummy2.next = head
+        curr = dummy2
+        while curr.next:
+            prev = dummy
+            while prev.next and curr.next.val > prev.next.val:
+                prev = prev.next
+            
+            # Placing the element
+            next_node = prev.next
+            prev.next = curr.next
+            another_next_node = curr.next.next
+            curr.next.next = next_node
+            curr.next = another_next_node
 
-        return head
+        return dummy.next
