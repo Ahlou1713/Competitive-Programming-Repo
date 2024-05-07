b14df96ee7df636219c08e8cff5a0d5d375ed288
Ahlou1713 <98119853+Ahlou1713@users.noreply.github.com>
2024-05-07 19:16:54 +0100

    Time: 151 ms (42.28%) | Memory: 18.3 MB (62.46%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -4,8 +4,6 @@ class Solution:
         for e in s:
             if stack and e == '*':
                 stack.pop()
-            elif e == '*':
-                continue
             else:
                 stack.append(e)
         
