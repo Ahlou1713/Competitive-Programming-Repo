9fc3d6a9558530893b607605f06349c2527b67b7
Ahlou1713 <98119853+Ahlou1713@users.noreply.github.com>
2024-06-13 20:42:39 +0100

    Time: 929 ms (58.48%) | Memory: 54.7 MB (76.65%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
@@ -6,9 +6,4 @@ class Solution:
         for _, to in edges:
             indegree[to] += 1
 
-        output = []
-        for i in range(n):
-            if indegree[i] == 0:
-                output.append(i)
-
-        return output
+        return [i for i in range(n) if indegree[i] == 0]
