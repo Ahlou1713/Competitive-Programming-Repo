c9523c2a31fca9724f059330973e5ad6bd5f9eca
Ahlou1713 <98119853+Ahlou1713@users.noreply.github.com>
2024-06-14 23:50:15 +0100

    Time: 588 ms (85.02%) | Memory: 21.5 MB (67.68%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,17 +1,15 @@
-from collections import defaultdict
-
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         
-
-        trusts = defaultdict(lambda: [0, 0])
+        indegree = [0] * n
+        outdegree = [0] * n
 
         for a, b in trust:
-            trusts[a][0] += 1
-            trusts[b][1] += 1
+            outdegree[a-1] += 1
+            indegree[b-1] += 1
 
         for person in range(1, n+1):
-            if trusts[person] == [0,n-1]:
+            if outdegree[person-1] == 0 and indegree[person-1] == n-1:
                 return person
         
         return -1
