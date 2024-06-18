8ea9d981d37fa10ea34c1f2757133b6898a6b64f
Ahlou1713 <98119853+Ahlou1713@users.noreply.github.com>
2024-06-18 19:44:32 +0100

    Time: 575 ms (5.01%) | Memory: 18.4 MB (14.40%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,12 +1,41 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        perimeter = 0
+        # perimeter = 0
+        # for i in range(len(grid)):
+        #     for j in range(len(grid[0])):
+        #         if grid[i][j] == 1:
+        #             perimeter += 4
+        #             if i > 0 and grid[i-1][j] == 1:
+        #                 perimeter -= 2
+        #             if j > 0 and grid[i][j-1] == 1:
+        #                 perimeter -= 2
+        # return perimeter
+
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        visited = [[False for i in range(len(grid[0]))] for j in range(len(grid))]
+
+        def inbound(row, col):
+            return (0 <= row < len(grid) and 0 <= col < len(grid[0]))
+        
+        def dfs(grid, visited, row, col):
+            # base case
+            visited[row][col] = True
+
+            perimeter = 4
+            for row_change, col_change in directions:
+                new_row = row + row_change
+                new_col = col + col_change
+                if inbound(new_row, new_col) and grid[new_row][new_col]:
+                    perimeter -= 1
+                    if not visited[new_row][new_col]:
+                        perimeter += dfs(grid, visited, new_row, new_col)
+
+            return perimeter
+
         for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    perimeter += 4
-                    if i > 0 and grid[i-1][j] == 1:
-                        perimeter -= 2
-                    if j > 0 and grid[i][j-1] == 1:
-                        perimeter -= 2
-        return perimeter
+            for j in range(len(grid[0])) :
+                if grid[i][j]:
+                    return dfs(grid, visited, i, j)
+        
+        return 0
+
