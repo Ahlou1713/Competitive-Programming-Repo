f851c4ab060f884e01d2f9b2a65a09719c3c036c
Ahlou1713 <98119853+Ahlou1713@users.noreply.github.com>
2024-07-02 19:03:40 +0100

    Time: 84 ms (5.16%) | Memory: 17.1 MB (14.46%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+
+        n = len(rooms)
+
+        for i in range(n):
+            graph[i] = rooms[i]
+
+        print(graph)
+        visited = []
+        queue = deque([0])
+        save = [0]
+
+        while queue:
+            node = queue.popleft()
+
+            if node not in visited:
+                visited.append(node)
+
+                for neighbour in graph[node]:
+                    if neighbour != node:
+                        queue.append(neighbour)
+                        save.append(neighbour)
+
+        for key in range(n):
+            if key not in save:
+                return False
+
+        
+        return True
