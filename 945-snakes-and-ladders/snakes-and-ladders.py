e0bd0f5b890c692bc2070f871bcb652ac466c22e
Ahlou1713 <98119853+Ahlou1713@users.noreply.github.com>
2024-08-08 23:36:55 +0100

    Time: 105 ms (37.70%) | Memory: 16.6 MB (89.94%) - LeetSync

---
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -0,0 +1,27 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        n = len(board)
+
+        cells = [None] * (n**2 + 1)
+        label = 1
+        columns = list(range(0, n))
+        for row in range(n - 1, -1, -1):
+            for column in columns:
+                cells[label] = (row, column)
+                label += 1
+            columns.reverse()
+
+        dist = [-1] * (n**2 + 1)
+        q = deque([1])
+        dist[1] = 0
+        while q:
+            curr = q.popleft()
+            for next in range(curr + 1, min(curr + 6, n**2) + 1):
+                row, column = cells[next]
+                if board[row][column] != -1:
+                    next = board[row][column]
+
+                if dist[next] == -1:
+                    dist[next] = dist[curr] + 1
+                    q.append(next)
+        return dist[n * n]
